48c35f5d2ce94bbf3f8ec65d2e63b4abc0870fb0
Suraj chauhan <surajssc1232@gmail.com>
2024-04-10 11:32:34 +0530

    idk man

--- src/main/java/org/example/shoppingapp/Entity/Inventory.java
+++ src/main/java/org/example/shoppingapp/Entity/Inventory.java
@@ -8,7 +8,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
-import org.springframework.boot.autoconfigure.web.WebProperties;
 
 @Data
 @Entity
--- src/main/java/org/example/shoppingapp/ShoppingAppApplication.java
+++ src/main/java/org/example/shoppingapp/ShoppingAppApplication.java
@@ -1,13 +1,11 @@
 package org.example.shoppingapp;
 
-import org.example.shoppingapp.Entity.Inventory;
-import org.example.shoppingapp.Service.InventoryServic;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
+
+
 
 @SpringBootApplication
 public class ShoppingAppApplication {
